ba562de7d9a35952bd98bff10bae787d1052ad10
Vladgobelen <vladgobelen@gmail.com>
2023-03-29 04:10:53 +0700

    заготовка под аддон

--- developer.lua
+++ developer.lua
@@ -3,16 +3,18 @@ GC_Sniffer:RegisterEvent("CHAT_MSG_GUILD")
 GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
     local nik=sender
     local str = string.gsub(message, "%s+", "")
-    if str == "ВОЖДЬ") then
-        SendChatMessage(nik .. ", простой или сложный?", "guild", nil, 1)
-    elseif string.find (message, "ВОЖДЬ сдать") then
-        SendChatMessage(nik .. ", опыт или деньги?", "guild", nil, 1)
-    elseif string.find (message, "ВОЖДЬ опыт") then
-        SendChatMessage(nik .. " получает 2 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
-    elseif string.find (message, "ВОЖДЬ деньги") then
-        SendChatMessage(nik .. " получит [стандартная награда для gildLvl_nik] реализую позже", "guild", nil, 1)
-        SendChatMessage(nik .. " получает 1 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
-    else
+    if (nik ~= "Двацветок" and nik ~= "Витинари" and nik ~= "Железобетонс" and nik ~= "Детрит") then
+        if str == "ВОЖДЬ" then
+            SendChatMessage(nik .. ", простой или сложный?", "guild", nil, 1)
+        elseif string.find (message, "ВОЖДЬ сдать") then
+            SendChatMessage(nik .. ", опыт или деньги?", "guild", nil, 1)
+        elseif string.find (message, "ВОЖДЬ опыт") then
+            SendChatMessage(nik .. " получает 2 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
+        elseif string.find (message, "ВОЖДЬ деньги") then
+            SendChatMessage(nik .. " получит [стандартная награда для gildLvl_nik] реализую позже", "guild", nil, 1)
+            SendChatMessage(nik .. " получает 1 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
+        else
+        end
     end
 
 end
