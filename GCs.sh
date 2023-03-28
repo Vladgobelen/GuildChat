a391382b2ea76676fdb827160176da89b3c66233
Vladgobelen <vladgobelen@gmail.com>
2023-03-28 22:44:49 +0700

    результат в kdialog

--- GCs.sh
+++ GCs.sh
@@ -5,7 +5,9 @@ tesseract /home/diver/Скрипты/scr/GC/0.png /home/diver/Скрипты/mod
 cat /home/diver/Скрипты/mods/tesseract/rus.traineddata.txt |grep ВОЖДЬ | awk '{print $2}' |tr -d \[]: |awk -v RS="\0" '{print $NF}' > /home/diver/Скрипты/temp/GC/res;
 echo " опыт или деньги?" >>/home/diver/Скрипты/temp/GC/res;
 cat /home/diver/Скрипты/temp/GC/res | tr "\n" "," | rev | cut -c2- | rev | xclip -sel clip;
-kdialog --msgbox "готово"
+xclip -sel clip -o >/home/diver/Скрипты/temp/GC/xres
+j=$(cat /home/diver/Скрипты/temp/GC/xres);
+kdialog --msgbox $j;
 xdotool getmouselocation --shell | grep WINDOW | awk -F "=" '{print $2}' > /home/diver/Скрипты/temp/win;
 i=$(cat /home/diver/Скрипты/temp/win);
 xdotool windowfocus $i;
