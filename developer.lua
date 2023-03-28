3f2061ece7cc5e787cd00976ccf508b88224cb32
Vladgobelen <vladgobelen@gmail.com>
2023-03-29 03:56:09 +0700

    заготовка под аддон

---
+++ developer.lua
@@ -0,0 +1,29 @@
+local GC_Sniffer = CreateFrame("Frame")
+GC_Sniffer:RegisterEvent("CHAT_MSG_GUILD")
+GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
+    local nik=sender
+    print(sender)
+    print(message)
+    local str
+    str = string.gsub(message, "%s+", "")
+    if str == "ВОЖДЬ") then
+        SendChatMessage(nik .. ", простой или сложный?", "guild", nil, 1)
+    else
+    end
+    if string.find (message, "ВОЖДЬ сдать") then
+        SendChatMessage(nik .. ", опыт или деньги?", "guild", nil, 1)
+    else
+    end
+    if string.find (message, "ВОЖДЬ опыт") then
+        SendChatMessage(nik .. " получает 2 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
+    else
+    end
+    if string.find (message, "ВОЖДЬ деньги") then
+        SendChatMessage(nik .. " получит [стандартная награда для gildLvl_nik] реализую позже", "guild", nil, 1)
+        SendChatMessage(nik .. " получает 1 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
+    else
+    end
+
+end
+
+)
