4e3ef4e27d95c3e1851ef922a3907cd5b89bdf15
Vladgobelen <vladgobelen@gmail.com>
2023-03-31 23:47:08 +0700

    полностью рабочая версия повышения через команду(точно рабочая с функцией и остановкой опыта на 9 лвле

--- expp.lua
+++ expp.lua
@@ -7,7 +7,6 @@ if (nik == "Двацветок" or nik == "Витинари" or nik == "Желе
     function all_trim(s)
         return s:match( "^%s*(.-)%s*$" )
     end
-
     function mysplit (inputstr, sep)
         if sep == nil then
                 sep = "%s"
@@ -18,28 +17,28 @@ if (nik == "Двацветок" or nik == "Витинари" or nik == "Желе
         end
         return t
     end
-
-    function lvlAgg (expPp, gExpN, gExpN1, gLvl, gNik, zT, gExpf)
-    local gExpN = gExpf - gExpN   ---получаем количество оставшегося лвла на уровне
+    function lvlAgg (expPp, gExpN1, gLvl, gNik,strokaFn)
+    local gExpNo = 2 ^ (gLvl + 2) - gExpN1   ---получаем количество оставшегося лвла на уровне
         for i = 1,expPp do               --прибавляем опыт по единичке
             gExpN1 = gExpN1 + 1     --единичку прибавили к опыту на уровне
-            gExpN = gExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-            if gExpN == 0 then       --если опыта на уровне не осталось, то
+            gExpNo = gExpNo - 1      -- от оставшегося опыта на уровне отнимаем единичку
+            if gExpNo == 0 then       --если опыта на уровне не осталось, то
                 gLvl = gLvl + 1  --прибавляем к лвлу единицу
                 SendChatMessage("Поздравляем " .. gNik .. " c лвлапом!!!", "guild", nil, 1)
-                gExpN = gExpNfN            --оставшийся опыт назначаем равным 8
+                gExpNo = 2 ^ (gLvl + 1) - gExpN1            --оставшийся опыт назначаем равным 8
                 gExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
             else
             end
         end
+        local strokaSplit = mysplit (strokaFn)
+        if gLvl == 9 then
+            SendChatMessage("Красочные спецэффекты вжу-вжу-пиу!фрррр! " .. gNik .. " получает доступ к квесту на получение офицерского звания", "guild", nil, 1)
+            gExpN1 = 0
+        else
+        end
+        gildExpN13 = string.format("%03d",gExpN1)
+        return (gLvl .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3])
     end
-    local stroka = officerNote
-    local strokaSplit = mysplit (stroka)
-    gildExpN13 = string.format("%03d",gExpN1)
-    stroka = gLvl .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-    GuildRosterSetOfficerNote(zT, stroka)
-    end
-
     local msgZ = all_trim (message);
     msgZ = mysplit (msgZ)
 
@@ -54,169 +53,35 @@ if (nik == "Двацветок" or nik == "Витинари" or nik == "Желе
                 local gildLvlN = tonumber (gildLvl)
                 local gildExpN1 = tonumber (gildExp)    --переводим опыт в число
                 if msgZ3n <= 8 then                       --нельзя прибавлять заодин раз больше 8 опыта
-                    local gildExpf0 = 8
-                    local gildExpf1 = 16
-                    local gildExpf2 = 32
-                    local gildExpf3 = 64
-                    local gildExpf4 = 128
-                    local gildExpf5 = 256
-                    local gildExpf6 = 512
-                    local gildExpf7 = 1024
-                    local gildExpf8 = 2048
+                    local strokaF = officerNote
                     if gildLvl == "0" then              --если текущий лвл нулевой, то
-                        lvlAgg (msgZ3n,gildExpN,gildExpN1,gildLvlN,msgZ[1],Zc,gildExpf0,)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "1" then
-                        local gildExpN = 8 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 16            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%03d",gildExpN1)
 
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "2" then
-                        local gildExpN = 16 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 32            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%03d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "3" then
-                        local gildExpN = 32 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 64            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%03d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "4" then
-                        local gildExpN = 64 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 128            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%03d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "5" then
-                        local gildExpN = 128 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 256            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%03d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "6" then
-                        local gildExpN = 256 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 512            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%03d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "7" then
-                        local gildExpN = 512 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. " c лвлапом!!!", "guild", nil, 1)
-                                gildExpN = 1024            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%04d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     elseif gildLvl == "8" then
-                        local gildExpN = 1024 - gildExpN   ---получаем количество оставшегося лвла на уровне
-                        for i = 1,msgZ3n do               --прибавляем опыт по единичке
-                            gildExpN1 = gildExpN1 + 1     --единичку прибавили к опыту на уровне
-                            gildExpN = gildExpN - 1      -- от оставшегося опыта на уровне отнимаем единичку
-                            if gildExpN == 0 then       --если опыта на уровне не осталось, то
-                                gildLvlN = gildLvlN + 1  --прибавляем к лвлу единицу
-                                SendChatMessage("Поздравляем " .. msgZ[1] .. ". Он может получить квест на повышение до офицера", "guild", nil, 1)
-                                gildExpN = 2048            --оставшийся опыт назначаем равным 8
-                                gildExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
-                            else
-                            end
-                        end
-                        local stroka = officerNote
-                        local strokaSplit = mysplit (stroka)
-                        gildExpN13 = string.format("%04d",gildExpN1)
-
-                        stroka = gildLvlN .. gildExpN13 .. " " .. strokaSplit[2] .. " " .. strokaSplit[3]
-                        GuildRosterSetOfficerNote(Zc, stroka)
+                        nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                        GuildRosterSetOfficerNote(Zc, nuovoStr)
                     end---конец проверки какой гильдлвл обрабатываем
                 else
                     print ("За один раз не больше 8 опыта")
