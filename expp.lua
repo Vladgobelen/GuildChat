be5a77c92e6e32dd908fa9754e8d74dfbd09d66c
Vladgobelen <vladgobelen@gmail.com>
2023-04-03 09:06:25 +0700

    exрр вроде как завершен

--- expp.lua
+++ expp.lua
@@ -56,41 +56,41 @@ if (nik == "Двацветок" or nik == "Витинари" or nik == "Желе
         end
         return (gLvl .. gildExpN13 .. " " .. strDen .. " " .. strokaSplit[3])
     end
-    function testNil (msg,count,str)
+    function testNil (msg,count,str,f)
         for i=1, count do
                 if msg[i] == nil then
                         table.insert(msg, i, str)
                 end
         end
+        if f ~= 0 then
+                msg = (table.concat(msg, " "))
+                msg = all_trim (msg)
+        end
         return msg
     end
     local msgZ = all_trim (message);
     msgZ = mysplit (msgZ)
-
     if string.find (message, "!получает") and string.find (message, msgZ[1]) and string.find (message, "опыта") then
         for Zc=1,GetNumGuildMembers(true) do
             local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(Zc)
             local msgZ3n = tonumber (msgZ[3])
             if name == msgZ[1] then
-                local gildLvl = string.sub(officerNote, 1, 1)
-                local gildExp = string.sub(officerNote, 2, 5)
+                strokaF = mysplit (officerNote)
+                strokaF =  testNil (officerNote,3,"0000",1)
+                local gildLvl = string.sub(strokaF, 1, 1)
+                local gildExp = string.sub(strokaF, 2, 5)
                 local rarGilvl = gildLvl .. gildExp
-                local gildExpN = tonumber (gildExp)
-                local gildLvlN = tonumber (gildLvl)
-                local gildExpN1 = tonumber (gildExp)    --переводим опыт в число
-                if msgZ3n <= 2 ^ (gildLvlN + 3) then                       --нельзя прибавлять заодин раз больше 8 опыта
-                    strokaF = officerNote
-                    strokaFM = mysplit (strokaF)
-                    strokaFM = testNil (strokaFM,3,"**")
-                    strokaF = strokaFM[1] .. " " .. strokaFM[2] .. " " .. strokaFM[3]
+                gildExpN = tonumber (gildExp)
+                gildLvlN = tonumber (gildLvl)
+                local gildExpN1 = tonumber (gildExp)
+                if msgZ3n <= 2 ^ (gildLvlN + 3) then
                     nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
                     GuildRosterSetOfficerNote(Zc, nuovoStr)
                     nuovoStrExp = mysplit (nuovoStr)
-                    SendChatMessage(rarGilvl .. ">>" .. nuovoStrExp[1], "guild", nil, 1)
+                    SendChatMessage(rarGilvl .. ">> " .. nuovoStrExp[1], "guild", nil, 1)
                 else
                     local ahtung = 2 ^ (gildLvlN + 3)
                     SendChatMessage("Можно не больше, чем " .. ahtung .. " опыта за один раз!!!" , "guild", nil, 1)
-
                 end
             else--конец проверки ника
             end--конец проверки ника
