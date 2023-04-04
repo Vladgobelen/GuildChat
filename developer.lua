f0f2f8263c6bfcffe5d6cffb7ec1cb5afce71045
Vladgobelen <vladgobelen@gmail.com>
2023-04-05 05:56:39 +0700

    квест рабочее с проверкой на количество доступных квестов, статус вывода в develop

--- developer.lua
+++ developer.lua
@@ -4,8 +4,22 @@ GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
 --команды для управления квестами
 local nik=sender
 local str = string.gsub(message, "%s+", "")
-if (nik ~= "Двацветок" and nik ~= "Витинари" and nik ~= "Железобетонс" and nik ~= "Детрит") then
-    if str == "ВОЖДЬ" then
+function all_trim(s)
+        return s:match( "^%s*(.-)%s*$" )
+end
+function mysplit (inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={}
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                table.insert(t, str)
+        end
+        return t
+end
+local nachalo = string.sub(message, 1, 1)
+
+    if str == "ВОЖДЬ" and nachalo~="*" then
         for guokZ=1,GetNumGuildMembers(true) do
             local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(guokZ)
             if name == sender then
@@ -21,93 +35,108 @@ if (nik ~= "Двацветок" and nik ~= "Витинари" and nik ~= "Жел
                 gildLvl = tonumber (gildLvl)
                 if denn == denT then
                    if qN >= gildLvl then
-                        SendChatMessage(name .. ", следущий квест на твоем гильдлвле доступен завтра.", "guild", nil, 1)
+                        SendChatMessage("*" .. name .. ", следущий квест на твоем гильдлвле доступен завтра.", "guild", nil, 1)
                    else
-                        SendChatMessage(nik .. ", простой или сложный?", "guild", nil, 1)
+                        SendChatMessage("*" .. nik .. ", простой или сложный?", "guild", nil, 1)
                    end
                 else
-                    SendChatMessage(nik .. ", простой или сложный?", "guild", nil, 1)
+                    SendChatMessage("*" .. nik .. ", простой или сложный?", "guild", nil, 1)
                 end
             else
             end
         end
-    elseif string.find (message, "ВОЖДЬ сдать") then
-        SendChatMessage(nik .. ", опыт или деньги?", "guild", nil, 1)
-    elseif string.find (message, "ВОЖДЬ опыт") then
-        SendChatMessage(nik .. " получает 2 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
-    elseif string.find (message, "ВОЖДЬ деньги") then
-        SendChatMessage(nik .. " получит [стандартная награда для gildLvl_nik] реализую позже", "guild", nil, 1)
-        SendChatMessage(nik .. " получает 1 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
+    elseif string.find (message, "ВОЖДЬ сдать") and nachalo~="*" then
+        SendChatMessage("*" .. nik .. ", опыт или деньги?", "guild", nil, 1)
+    elseif string.find (message, "ВОЖДЬ опыт") and nachalo~="*" then
+        SendChatMessage("*" .. nik .. " получает 2 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
+    elseif string.find (message, "ВОЖДЬ деньги") and nachalo~="*" then
+        SendChatMessage("*" .. nik .. " получит [стандартная награда для gildLvl_nik] реализую позже", "guild", nil, 1)
+        SendChatMessage("*" .. nik .. " получает 1 опыта." .. " До лвлапа осталось [заглушка сделаю позже]", "guild", nil, 1)
     else
     end
     --возможность игроку писать свою заметку
-    if string.find (message, "!заметка") then
+    testHis=mysplit(message)
+    testHis[1]=all_trim(testHis[1])
+    if string.find (message, "!заметка") and testHis[1] == "!заметка" and nachalo~="*" then
         local vz = (message):gsub("!заметка ", "");
         for z=1,GetNumGuildMembers(true) do
             local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(z)
             if name == sender then
                 GuildRosterSetPublicNote(z, vz)
+                SendChatMessage("*" .. name .. ", заметка добавлена", "guild", nil, 1)
             else
             end
         end
-    else
+    end
+    if string.find (message, "!заметка+") and testHis[1] == "!заметка+" and nachalo~="*" then
+        for z=1,GetNumGuildMembers(true) do
+            local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(z)
+            if name == sender then
+                local vzP = (message):gsub("!заметка%+", "");
+                vzP=all_trim(vzP)
+                local zamDop = publicNote .. " " .. vzP
+                GuildRosterSetPublicNote(z, zamDop)
+                SendChatMessage("*" .. name .. ", заметка дополнена", "guild", nil, 1)
+            end
+        end
     end
     --справка
-    if string.find (message, "ВОЖДЬ инфо") then
-        SendChatMessage("Получить квест:     ВОЖДЬ", "guild", nil, 1)
-        SendChatMessage("Сдать квест:        ВОЖДЬ сдать", "guild", nil, 1)
-        SendChatMessage("Заполнить заметку:  !заметка Текст заметки" , "guild", nil, 1)
-        SendChatMessage("Еще больше команд хороших и разных тут: https://t.me/AnkMorporkInfo", "guild", nil, 1)
+    if string.find (message, "ВОЖДЬ инфо") and nachalo~="*" then
+        SendChatMessage("*Получить квест:     ВОЖДЬ", "guild", nil, 1)
+        SendChatMessage("*Сдать квест:        ВОЖДЬ сдать", "guild", nil, 1)
+        SendChatMessage("*Заполнить заметку:  !заметка Текст заметки" , "guild", nil, 1)
+        SendChatMessage("*Дополнить существующую заметку:  !заметка+ Текст заметки" , "guild", nil, 1)
+        SendChatMessage("*Еще больше команд хороших и разных тут: https://t.me/AnkMorporkInfo", "guild", nil, 1)
     else
     end
     --показать инфу об игроке
     local guokInf = string.gsub(message, "%s+", "")
-    if string.find (guokInf, "!гильдлвл") then
+    if string.find (guokInf, "!гильдлвл") and nachalo~="*" then
         for guok=1,GetNumGuildMembers(true) do
             local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(guok)
             if name == sender then
                 local gildLvl = string.sub(officerNote, 1, 1)
                 local gildExp = string.sub(officerNote, 2, 5)
-                SendChatMessage(sender .. ": " .. gildLvl .. " гильдлвл" .. " и " .. gildExp .. " опыта", "guild", nil, 1)
+                SendChatMessage("*" .. sender .. ": " .. gildLvl .. " гильдлвл" .. " и " .. gildExp .. " опыта", "guild", nil, 1)
                 local gildLvlN = tonumber (gildLvl)
                 local gildExpN = tonumber (gildExp)
                 if gildLvl == "0" then
                     local gildExpN = 4 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "1" then
                     local gildExpN = 8 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "2" then
                     local gildExpN = 16 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "3" then
                     local gildExpN = 32 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "4" then
                     local gildExpN = 64 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "5" then
                     local gildExpN = 128 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "6" then
                     local gildExpN = 256 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "7" then
                     local gildExpN = 512 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "8" then
                     local gildExpN = 1024 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 elseif gildLvl == "9" then
                     local gildExpN = 2048 - gildExpN
-                    SendChatMessage(sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
+                    SendChatMessage("*" .. sender .. ": до лвлапа осталось " .. gildExpN .. " опыта" , "guild", nil, 1)
                 end
             else
             end
         end
     else
     end
-end
+
 
 
 
@@ -116,7 +145,7 @@ end
 
     --запись о прогрессе в информацию о персонаже (заготовка)
     local str2 = string.gsub(message, "%s+", "")
-    if string.find (str2, "mostrareID") then
+    if string.find (str2, "mostrareID") and nachalo~="*" then
         for i=1,GetNumGuildMembers(true) do
             local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(i)
 
--- quests/quests.lua
+++ quests/quests.lua
@@ -25,31 +25,48 @@ local testHis = string.sub(message, 1, 1)
 
 if string.find (message, "!старт квест") and testHis~="*" then
 	msg = mysplit(message)
-
+		
+	if msg[3]==nil then
+			SendChatMessage("*" .. nik .. ", в какой мир ты хочешь попасть? Ау, не вижу названия мира!", "guild", nil, 1)
+	else
+	end
 	quest = msg[3] 	
-	if TDGq==nil then
-		TDGq={}
-	end
-	if TDGq[quest]==nil then
-		TDGq[quest]={}
-	end
-	if TDGq[quest]["создатель"]==nil then
-		TDGq[quest]["создатель"]=nik
-	end	
-	if TDG[nik]==nil then
-		TDG[nik]={}
-	end
-	TDG[nik]["текущий_квест"]=quest
-	if TDGq[quest]["стартовая_локация"]==nil or TDGq[quest]["стартовая_локация"]==0 and TDGq[quest]["создатель"]==nik then
-		TDGq[quest]["стартовая_локация"]=0
-		SendChatMessage("*Вселенная была пуста и безвидна. И увидел " .. nik .. ", что нужно !создать Мир.", "guild", nil, 1);
-	elseif TDGq[quest]["стартовая_локация"]==nil or TDGq[quest]["стартовая_локация"]==0 and TDGq[quest]["создатель"]~=nik then
-		SendChatMessage("*" .. nik .. ", как то пусто тут. Лучше подождать пока что то будет создано или создать свое.. ", "guild", nil, 1)
-	elseif TDGq[quest]["стартовая_локация"]~=nil or TDGq[quest]["стартовая_локация"]~=0 and TDGq[quest]["создатель"]==nik then
+	if TDG[nik]["доступно_квестов"]~=0 then
+		if TDGq==nil then
+			TDGq={}
+		end
+		if TDGq[quest]==nil then
+			TDGq[quest]={}
+		end
+		if TDGq[quest]["создатель"]==nil then
+			TDGq[quest]["создатель"]=nik
+			TDG[nik]["доступно_квестов"]=0
+		end	
+		if TDG[nik]==nil then
+			TDG[nik]={}
+		end
+		TDG[nik]["текущий_квест"]=quest
+		if TDGq[quest]["стартовая_локация"]==nil or TDGq[quest]["стартовая_локация"]==0 and TDGq[quest]["создатель"]==nik then
+			TDGq[quest]["стартовая_локация"]=0
+			SendChatMessage("*Вселенная была пуста и безвидна. И увидел " .. nik .. ", что нужно !создать Мир.", "guild", nil, 1);
+		end
+		if TDGq[quest]["стартовая_локация"]~=nil or TDGq[quest]["стартовая_локация"]~=0 and TDGq[quest]["создатель"]==nik then
 			
-		SendChatMessage("*" .. nik .. ", тут старт квеста. ", "guild", nil, 1)		
+			SendChatMessage("*" .. nik .. ", тут нужно вывести описание стартовой локации", "guild", nil, 1)		
+		end	
 	end
-
+		if TDG[nik]["доступно_квестов"]==0 then 
+			if TDGq[quest]["стартовая_локация"]==nil or TDGq[quest]["стартовая_локация"]==0 and TDGq[quest]["создатель"]~=nik then
+				SendChatMessage("*" .. nik .. ", как то пусто тут. Лучше подождать пока что то будет создано или создать свое.. ", "guild", nil, 1)
+			end
+			if TDGq[quest]["стартовая_локация"]==nil or TDGq[quest]["стартовая_локация"]==0 and TDGq[quest]["создатель"]==nik then
+				SendChatMessage("*" .. nik .. ", может все таки что то создать уже?", "guild", nil, 1)
+			end
+			if TDGq[quest]["стартовая_локация"]~=nil and TDG[quest]["стартовая_локация"]~=0 and TDGq[quest]["создатель"]==nik then
+				
+				SendChatMessage("*" .. nik .. ", 111тут нужно вывести описание стартовой локации", "guild", nil, 1)	
+			end	
+		end
 
 end
 quest=TDG[nik]["текущий_квест"]
