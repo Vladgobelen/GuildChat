17894704de0e7e1157b6782f5a762baa9e850197
Vladgobelen <vladgobelen@gmail.com>
2023-04-20 11:04:15 +0700

    второй вариант прибавки опыта (без повышения даты)

--- expp.lua
+++ expp.lua
@@ -4,27 +4,16 @@ GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
 --добавление опыта персонажу командой
 local nik = sender
 if (nik == "Двацветок" or nik == "Витинари" or nik == "Железобетонс" or nik == "Детрит") then
-    function all_trim(s)
-        return s:match( "^%s*(.-)%s*$" )
-    end
-    function mysplit (inputstr, sep)
-        if sep == nil then
-                sep = "%s"
-        end
-        local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-                table.insert(t, str)
-        end
-        return t
-    end
-
-    local msgZ = all_trim (message);
-    msgZ = mysplit (msgZ)
-    if string.find (message, "!получает") and string.find (message, msgZ[1]) and string.find (message, "опыта") then
+    msgExpPP = mysplit (message)
+    msgZ1=msgExpPP[1]
+    msgZ2=msgExpPP[2]
+    msgZ3=msgExpPP[3]
+    msgZ4=msgExpPP[4]
+    if msgZ2=="!получает" and msgZ4=="опыта" then
         for Zc=1,GetNumGuildMembers(true) do
             local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(Zc)
-            local msgZ3n = tonumber (msgZ[3])
-            if name == msgZ[1] then
+            local msgZ3n = tonumber (msgZ3)
+            if name == msgZ1 then
                 strokaF = mysplit (officerNote)
                 strokaF =  testNil (strokaF,3,"0000",1)
                 local gildLvl = string.sub(strokaF, 1, 1)
@@ -34,7 +23,7 @@ if (nik == "Двацветок" or nik == "Витинари" or nik == "Желе
                 gildLvlN = tonumber (gildLvl)
                 local gildExpN1 = tonumber (gildExp)
                 if msgZ3n <= 2 ^ (gildLvlN + 3) then
-                    nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ[1],strokaF)
+                    nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ1,strokaF)
                     GuildRosterSetOfficerNote(Zc, nuovoStr)
 
                 else
@@ -44,7 +33,41 @@ if (nik == "Двацветок" or nik == "Витинари" or nik == "Желе
             else--конец проверки ника
             end--конец проверки ника
         end--конец чтения данных из гильдии
-    else--конец проверки ключевого слова ник получает
+    elseif msgZ2=="получает" and msgZ4=="опыта" then
+        for Zc=1,GetNumGuildMembers(true) do
+            local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(Zc)
+            local msgZ3n = tonumber (msgZ3)
+            if name == msgZ1 then
+                strokaF = mysplit (officerNote)
+                strokaF =  testNil (strokaF,3,"0000",1)
+                local gildLvl = string.sub(strokaF, 1, 1)
+                local gildExp = string.sub(strokaF, 2, 5)
+                local rarGilvl = gildLvl .. gildExp
+                gildExpN = tonumber (gildExp)
+                gildLvlN = tonumber (gildLvl)
+                local gildExpN1 = tonumber (gildExp)
+                if msgZ3n <= 2 ^ (gildLvlN + 3) then
+                    nuovoStr = lvlAgg (msgZ3n,gildExpN1,gildLvlN,msgZ1,strokaF)
+                    GuildRosterSetOfficerNote(Zc, nuovoStr)
+                otmenaDaty=mysplit(nuovoStr)
+                otmenaDaty1=otmenaDaty[1]
+                otmenaDaty2=otmenaDaty[2]
+                otmenaDaty3=otmenaDaty[3]
+                otmenaDaty4=string.sub(otmenaDaty2,1,2)
+                otmenaDaty5=string.sub(otmenaDaty2,3,3)
+                otmenaDaty5=otmenaDaty5-1
+                otmenaDaty6=otmenaDaty1 .. " " .. otmenaDaty4 .. otmenaDaty5 .. " " .. otmenaDaty3
+                GuildRosterSetOfficerNote(Zc, otmenaDaty6)
+                else
+                    local ahtung = 2 ^ (gildLvlN + 3)
+                    SendChatMessage("Можно не больше, чем " .. ahtung .. " опыта за один раз!!!" , "OFFICER", nil, 1)
+                end
+
+            else--конец проверки ника
+            end--конец проверки ника
+
+        end
+
     end--конец проверки ключевого слова  ник получает
 else---конец проверки ника на совпадение со мной
 end---конец проверки ника на совпадение со мной
