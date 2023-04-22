2644e28f1451fb0e1473ed128297fe99d6ccb166
Vladgobelen <vladgobelen@gmail.com>
2023-04-22 10:49:11 +0700

    Полный пересмотр подхода к программированию. Нужны тесты.

---
+++ developerS.lua
@@ -0,0 +1,355 @@
+local GC_Sniffer = CreateFrame("Frame")
+GC_Sniffer:RegisterEvent("CHAT_MSG_OFFICER")
+GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
+--команды для управления квестами
+local nik=sender
+local endQuests="простые_квесты"
+local qAchiv="взят_ли_квест_простая_ачивка"
+local timeLimit="время_последнего_обращения"
+local str = string.gsub(message, "%s+", "")
+local hshStran3S
+local hshStranMsgS
+local hshStranMsgS2
+local hshStranS1
+local hshStranS2
+local hshStranMsgS1
+hshStranMsgS=mysplit(message)
+
+hshStranMsgS2=string.sub(message,7,7)
+if string.find (message, "#") and hshStranMsgS[2]~= nil and nachalo~="*" then
+	hshStranMsgS1=hshStranMsgS[1]
+	hshStranS1,hshStranS2=hshStrNuovoServ(hshStranMsgS1,sender)
+
+end
+hshStran3S=hshSenderNomeServ(sender)
+nachaloStr = string.utf8sub(message, 1, 1)
+
+if TDG[sender]==nil then
+	TDG[sender]={}
+end
+if TDG[nik][endQuests] == nil then
+	TDG[nik][endQuests]={}
+end
+if pQuests == nil then
+	pQuests={}
+end
+if pQuests[1] == nil then
+	pQuests[1]={}
+end
+if pQuests[2] == nil then
+	pQuests[2]={}
+end
+if pQuests[3] == nil then
+	pQuests[3]={}
+end
+local nachalo = string.utf8sub(message, 1, 1)
+if hshStranS1==hshStranS2 and string.find (message, "#aab") then
+	msg1=mysplit(message)
+	msg2=msg1[6]
+	TDG[sender][endQuests][msg2]="1"
+	testQLVL=TDG[sender]["уровень_квестов"]
+	testQLVL=tonumber(testQLVL)
+	countQ=tablelength(pQuests[testQLVL])
+	chisloProstyhQComplit=0
+	chisloProstyhQComplit=tonumber(chisloProstyhQComplit)
+	for testQ=1, countQ do
+		x = math.random(1, countQ)
+		ach=pQuests[testQLVL][x]
+		if TDG[sender][endQuests][ach]~="1" then
+			SendChatMessage(hshStran3S .. " #aaa " .. sender .. ", покажи мне ачивку " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+			break
+		else
+			chisloProstyhQComplit=chisloProstyhQComplit + 1
+			if chisloProstyhQComplit==countQ then
+				testQLVL=TDG[sender]["уровень_квестов"]
+				testQLVL=tonumber(testQLVL)
+				testQLVL=testQLVL+1
+				TDG[sender]["уровень_квестов"]=testQLVL
+				SendChatMessage("*" .. sender .. ", сложность квестов повышена.", "OFFICER", nil, 1)
+			end
+		end
+	end
+end
+if hshStranS1==hshStranS2 and string.find (message, "#aac") then
+	vydat_q=mysplit(message)
+	if TDG[sender][qAchiv]==nil or TDG[sender][qAchiv]=="9999" then
+		SendChatMessage(hshStran3S .. " #aae " .. sender .. ", получи ачивку " .. vydat_q[5] .. " " .. GetAchievementLink(vydat_q[5]), "OFFICER", nil, 1)
+		prostye_q=vydat_q[5]
+		TDG[sender][endQuests][prostye_q]="0"
+		TDG[sender][qAchiv]=prostye_q
+	else
+		SendChatMessage(hshStran3S .. " #aad " .. sender .. ", у тебя уже взят квест: " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+	end
+end
+
+if hshStranS1==hshStranS2 and string.find(message, "#aag") then
+	msg=mysplit(message)
+	msg1=msg[4]
+	TDG[sender][endQuests][msg1]="1"
+	TDG[sender][qAchiv]="9999"
+	SendChatMessage(sender .. " !получает 3 опыта", "GUILD", nil, 1);
+end
+
+if string.find (sender, "Витинари") or string.find (sender, "Хэвлок") or string.find (sender, "Железобетонс") or string.find (sender, "Детрит") or string.find (sender, "Двацветок") then
+	if string.find (message, "!удалить ачивку") and nachalo~="*" then
+		msg=mysplit(message)
+		q=msg[3]
+		q=tonumber(q)
+		msg=msg[4]
+		for key, val in pairs(pQuests[q]) do
+			if val==msg then
+				table.remove(pQuests[q], key)
+				print("*Квест " .. val .. " " .. GetAchievementLink(val) .. " был удален.")
+			else
+			end
+		end
+	end
+end
+if sender=="Витинари" or sender=="Хэвлок" or sender=="Железобетонс" or sender=="Детрит" or sender=="Двацветок" then
+    if string.find (message, "!добавить ачивку") and nachalo~="*" then
+		soobshenie=mysplit(message)
+		urovenQuestov=soobshenie[3]
+		urovenQuestov=tonumber(urovenQuestov)
+		nomerQuesta=soobshenie[4]
+		for key, val in pairs(pQuests[urovenQuestov]) do
+			if val==nomerQuesta then
+				print("*Ачивка " .. val .. " " .. GetAchievementLink(val) .. " уже была добавлена.")
+				testIDqq=1
+			else
+			end
+		end
+		if testIDqq~=1 then
+			table.insert(pQuests[urovenQuestov], nomerQuesta)
+			print("*Ачивка " .. nomerQuesta .. " " .. GetAchievementLink(nomerQuesta) .. " была добавлена.")
+		end
+	end
+end
+
+
+
+if message == "ВОЖДЬ, простой!" or string.find (message, "#zzs") and nachalo~="*" then
+	if TDG[sender]==nil then
+		TDG[sender]={}
+	end
+	if TDG[sender][endQuests]==nil then
+		TDG[sender][endQuests]={}
+	end
+	local guokTimerStart=pQuests[1][1]
+	if TDG[sender]["уровень_квестов"]==nil then
+		TDG[sender]["уровень_квестов"]=1
+	end
+
+	for guokZ=1,GetNumGuildMembers(true) do
+		local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, guid = GetGuildRosterInfo(guokZ)
+		if name == sender then
+			local denT = string.utf8sub(officerNote, 6, 7)
+			local qN = string.utf8sub(officerNote, 8, 8)
+			local gildLvl = string.utf8sub(officerNote, 1, 1)
+			qN = tonumber (qN)
+			denn = {}
+			denn = date("%d")
+			denn = tonumber(denn)
+			denT = tonumber(denT)
+			gildLvl = tonumber (gildLvl)
+			if denn == denT then
+				if qN >= gildLvl then
+					testQLim="1"
+				else
+					testQLim="0"
+				end
+			else
+				testQLim="0"
+			end
+		else
+		end
+	end
+	if testQLim=="0" then
+		testQLVL=TDG[sender]["уровень_квестов"]
+		testQLVL=tonumber(testQLVL)
+		if testQLVL==1 then
+			if TDG[sender][endQuests][guokTimerStart]~=nil then
+				if TDG[sender][qAchiv]==nil or TDG[sender][qAchiv]=="9999" then
+					countQ=tablelength(pQuests[testQLVL])
+					local chisloProstyhQComplit=0
+					chisloProstyhQComplit=tonumber(chisloProstyhQComplit)
+					for testQ=1, countQ do
+						local x = math.random(1, countQ)
+						ach=pQuests[testQLVL][x]
+						if TDG[sender][endQuests][ach]~="1" then
+							SendChatMessage(hshStran3S .. " #aaa " .. sender .. ", покажи мне ачивку " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+							break
+						else
+							chisloProstyhQComplit=chisloProstyhQComplit + 1
+							if chisloProstyhQComplit==countQ then
+								testQLVL=TDG[sender]["уровень_квестов"]
+								testQLVL=tonumber(testQLVL)
+								testQLVL=testQLVL+1
+								TDG[sender]["уровень_квестов"]=testQLVL
+								SendChatMessage("*" .. sender .. ", сложность квестов повышена.", "OFFICER", nil, 1)
+							end
+						end
+					end
+				else
+					ach=TDG[sender][qAchiv]
+					SendChatMessage(hshStran3S .. " #aad " .. sender .. ", у тебя уже взят квест: " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+				end
+			else
+				countQ=tablelength(pQuests[testQLVL])
+					local chisloProstyhQComplit=0
+					chisloProstyhQComplit=tonumber(chisloProstyhQComplit)
+					for testQ=1, countQ do
+						local x = math.random(1, countQ)
+						ach=pQuests[testQLVL][x]
+						if TDG[sender][endQuests][ach]~="1" then
+							SendChatMessage(hshStran3S .. " #aaa " .. sender .. ", покажи мне ачивку " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+							break
+						else
+							chisloProstyhQComplit=chisloProstyhQComplit + 1
+							if chisloProstyhQComplit==countQ then
+								testQLVL=TDG[sender]["уровень_квестов"]
+								testQLVL=tonumber(testQLVL)
+								testQLVL=testQLVL+1
+								TDG[sender]["уровень_квестов"]=testQLVL
+								SendChatMessage("*" .. sender .. ", сложность квестов повышена.", "OFFICER", nil, 1)
+							end
+						end
+					end
+			end
+		elseif testQLVL==2 then
+			SendChatMessage("*" .. sender .. ", ты на втором лвле квестов.", "OFFICER", nil, 1)
+			if TDG[sender][qAchiv]==nil or TDG[sender][qAchiv]=="9999" then
+				countQ=tablelength(pQuests[testQLVL])
+				local chisloProstyhQComplit=0
+				chisloProstyhQComplit=tonumber(chisloProstyhQComplit)
+				j=0
+				k=0
+				for testQ=1, countQ do
+					local x = math.random(1, countQ)
+					ach=pQuests[testQLVL][x]
+
+
+
+					if TDG[sender][endQuests][ach]~="9999" then
+							SendChatMessage(hshStran3S .. " #aah " .. sender .. ", покажи ачивку " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+							break
+					else
+						chisloProstyhQComplit=chisloProstyhQComplit + 1
+						if chisloProstyhQComplit==countQ then
+							testQLVL=TDG[sender]["уровень_квестов"]
+							testQLVL=tonumber(testQLVL)
+							testQLVL=testQLVL+1
+							TDG[sender]["уровень_квестов"]=testQLVL
+							SendChatMessage("*" .. sender .. ", сложность квестов повышена.", "OFFICER", nil, 1)
+						end
+					end
+				end
+			else
+				ach=TDG[sender][qAchiv]
+				SendChatMessage(hshStran3S .. " #aad " .. sender .. ", у тебя уже взят квест: " .. ach .. " " .. GetAchievementLink(ach), "OFFICER", nil, 1)
+			end
+		end
+	else
+		SendChatMessage("*" .. sender .. ", следущий квест на твоем гильдлвле доступен завтра, но тебе доступен дополнительный квест с повышенным опытом: #zzz", "officer", nil, 1)
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if hshStranS1==hshStranS2 and string.find (message, "#aaj") and nachalo~="*" then
+	msgQLVL2Vydat=mysplit(message)
+	msgQLVL2Vydat2=msgQLVL2Vydat[8]
+	msgQLVL2Vydat2=tonumber(msgQLVL2Vydat2)
+	msgQLVL2Vydat3=msgQLVL2Vydat[9]
+	msgQLVL2Vydat3=tonumber(msgQLVL2Vydat3)
+	msgQLVL2Vydat4=msgQLVL2Vydat[6]
+	msgQLVL2Vydat4=tonumber(msgQLVL2Vydat4)
+	skolko_dolzhno_stat=msgQLVL2Vydat2-msgQLVL2Vydat4+3
+	if msgQLVL2Vydat4<=3 then
+		SendChatMessage(hshStran3S .. " #aak " .. sender .. ", получи полностью ачивку " .. msgQLVL2Vydat3 .. " " .. GetAchievementLink(msgQLVL2Vydat3), "officer", nil, 1)
+		TDG[sender]["взят_ли_квест_простая_ачивка"]=msgQLVL2Vydat3
+		TDG[sender][endQuests][msgQLVL2Vydat3]=msgQLVL2Vydat2
+	else
+		SendChatMessage(hshStran3S .. " #aal " .. sender .. ", получи еще три пункта ачивки " .. msgQLVL2Vydat3 .. " " .. GetAchievementLink(msgQLVL2Vydat3), "officer", nil, 1)
+		TDG[sender]["взят_ли_квест_простая_ачивка"]=msgQLVL2Vydat3
+		TDG[sender][endQuests][msgQLVL2Vydat3]=skolko_dolzhno_stat
+	end
+end
+
+
+
+if hshStranS1==hshStranS2 and string.find (message, "#zzk") and nachalo~="*" then
+	schitaemSlova=mysplit(message)
+	schitaemSlova=schitaemSlova[8]
+	TDG[sender][endQuests][schitaemSlova]="9999"
+	TDG[sender][qAchiv]="9999"
+	SendChatMessage(sender .. " !получает 3 опыта", "GUILD", nil, 1);
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if message == "ВОЖДЬ, сдать" or string.find (message, "#zzr") or string.find (message, "#aam") then
+	hours, minutes = GetGameTime()
+	timeProvMin=minutes
+	timeProvMin1=timeProvMin+1
+	if TDG[sender][timeLimit]~=timeProvMin and TDG[sender][timeLimit]~=timeProvMin1 then
+		if TDG[sender][qAchiv]==nil or TDG[sender][qAchiv]=="9999" then
+			SendChatMessage(hshStran3S .. " #aae " .. sender .. ", у тебя нет взятых квестов.", "OFFICER", nil, 1);
+		else
+			proverkaVypolneniya=TDG[sender][qAchiv]
+			SendChatMessage(hshStran3S .. " #aaf " .. sender .. ", а сделал ли ты " .. proverkaVypolneniya .. " " .. GetAchievementLink(proverkaVypolneniya) .. "?", "OFFICER", nil, 1)
+		end
+		TDG[sender][timeLimit]=minutes
+	else
+		SendChatMessage("*" .. sender .. ", попробуй через минуту.", "OFFICER", nil, 1)
+	end
+end
+
+
+
+if string.find (message, "#zzp") then
+	if TDG[sender]["взят_ли_квест_простая_ачивка"]==nil then
+		SendChatMessage(sender .. ", тебе нечего отменять.", "OFFICER", nil, 1)
+	else
+		testQuest=TDG[sender]["взят_ли_квест_простая_ачивка"]
+		if TDG[sender]["взят_ли_квест_простая_ачивка"]~="9999" then
+			SendChatMessage(hshStran3S .." #xxx" .. " " .. sender .. ", квест " .. testQuest .. " " .. GetAchievementLink(testQuest) .. " отменен.", "OFFICER", nil, 1)
+			TDG[sender]["взят_ли_квест_простая_ачивка"]="9999"
+		else
+			SendChatMessage(sender .. ", тебе нечего отменять.", "OFFICER", nil, 1)
+		end
+	end
+
+end
+end
+)
---
+++ functionsS.lua
@@ -0,0 +1,262 @@
+function mysplit (inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={}
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
+function all_trim(s)
+	return s:match( "^%s*(.-)%s*$" )
+end
+
+function kirTest(b,n1)
+test1=b:sub(n1,n1)
+testN=b:byte(7)
+testN=tonumber(testN)
+if testN == 208 then
+	r=b:sub(n1*2-1,n1*2)
+else
+	r=test1
+end
+return r
+end
+
+function alfabet (bookv)
+shablon="абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	myB=string.find(shablon,bookv)
+	return myB
+end
+
+function hashStr (nome)
+	local i = time()
+	i = i % 10000
+	i=tostring(i)
+	i=string.utf8sub(i,2,3)
+	i=string.format("%03d", i)
+	nomeLen=string.utf8len(nome)
+	nome1={}
+	for startLen=1,nomeLen do
+		nome1[startLen]=string.utf8sub(nome, startLen, startLen)
+		tmNome=nome1[startLen]
+		nome1[startLen]=alfabet(tmNome)
+	end
+	nome11=0
+	for startLen=1,nomeLen do
+		nome11=nome11+nome1[startLen]
+	end
+	nome11=nome11 % 1000
+	hNome=string.format("%03d",nome11)
+	iN=tostring(i)
+	r1=string.utf8sub(iN, 1, 1)
+	r2=string.utf8sub(hNome, 1, 1)
+	r3=string.utf8sub(iN, 2, 2)
+	r4=string.utf8sub(hNome, 2, 2)
+	r5=string.utf8sub(iN, 3, 3)
+	r6=string.utf8sub(hNome, 3, 3)
+	r=r1 .. r2 .. r3 .. r4 .. r5 .. r6
+	return r
+end
+
+function tablelength(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
+
+function lvlAgg (expPp, gExpN1, gLvl, gNik,strokaFn)
+    local gExpNo = 2 ^ (gLvl + 2) - gExpN1   ---получаем количество оставшегося лвла на уровне
+        rarExp=gExpN1
+        rarLvl=gLvl
+        rarExp=string.format("%03d",rarExp)
+        for i = 1,expPp do               --прибавляем опыт по единичке
+            gExpN1 = gExpN1 + 1     --единичку прибавили к опыту на уровне
+            gExpNo = gExpNo - 1      -- от оставшегося опыта на уровне отнимаем единичку
+            if gExpNo == 0 then       --если опыта на уровне не осталось, то
+                gLvl = gLvl + 1  --прибавляем к лвлу единицу
+                SendChatMessage("Поздравляем " .. gNik .. " c лвлапом!!!", "OFFICER", nil, 1)
+                gExpNo = 2 ^ (gLvl + 2)            --оставшийся опыт назначаем равным 8
+                gExpN1 = 0           --текущий опыт на уровне назначаем равным нулю
+            else
+            end
+
+        end
+        gExpN1Nuovo=string.format("%03d",gExpN1)
+        SendChatMessage(rarLvl .. rarExp .. " >> " .. gLvl .. gExpN1Nuovo .." До лвлапа осталось: " .. gExpNo .. " опыта", "OFFICER", nil, 1)
+        local strokaSplit = mysplit (strokaFn)
+        if gLvl == 9 then
+            SendChatMessage("Красочные спецэффекты вжу-вжу-пиу!фрррр! " .. gNik .. " получает доступ к квесту на получение офицерского звания", "OFFICER", nil, 1)
+            gExpN1 = 0
+        else
+        end
+        gildExpN13 = string.format("%03d",gExpN1)
+
+        local denT = string.sub(strokaSplit[2], 1, 2)
+        local qN = string.sub(strokaSplit[2], 3, 3)
+        denn ={}
+        denn=date("%d")
+        qN = tonumber (qN)
+        if denn ~= denT then
+            strDen = denn .. 1
+        else
+            if qN < gLvl then
+                qN = qN + 1
+                strDen = denn .. qN
+            else
+                SendChatMessage(gNik .. ", следущий квест на твоем гильдлвле доступен завтра.", "OFFICER", nil, 1)
+                strDen = denn .. qN
+            end
+        end
+        return (gLvl .. gildExpN13 .. " " .. strDen .. " " .. strokaSplit[3])
+    end
+    function testNil (msg,count,str,f)
+        for i=1, count do
+                if msg[i] == nil then
+                        table.insert(msg, i, str)
+                end
+        end
+        if f ~= 0 then
+                msg = (table.concat(msg, " "))
+                msg = all_trim (msg)
+        end
+        return msg
+    end
+
+function hshStrNuovoServ(hshRarS,mioNomeS)
+	local nomeLen
+	local tmNome
+	local nome11
+	local hNome
+	local hshMioNomeS1
+	local hshMioNomeS2
+	local hshMioNomeS3
+	local qioS
+	local hshRarS1
+	local hshRarS2
+	local hshRarS3
+	local hshRarSMshRezult
+	local hshMioNomeSRezult
+	nomeLen=string.utf8len(mioNomeS)
+	local nome1={}
+	for startLen=1,nomeLen do
+		nome1[startLen]=string.utf8sub(mioNomeS, startLen, startLen)
+		tmNome=nome1[startLen]
+		nome1[startLen]=alfabet(tmNome)
+	end
+	nome11=0
+	for startLen=1,nomeLen do
+		nome11=nome11+nome1[startLen]
+	end
+	nome11=nome11 % 1000
+	hNomeS=string.format("%03d",nome11)
+	hshMioNomeS1=string.sub(hNomeS,1,1)
+	hshMioNomeS2=string.sub(hNomeS,2,2)
+	hshMioNomeS3=string.sub(hNomeS,3,3)
+	qioS=string.sub(hshRarS,2,2)
+	qioS=tonumber(qioS)
+	if qioS==0 then
+		hshRarS1=string.sub(hshRarS, 3,3)
+		hshRarS2=string.sub(hshRarS, 4,4)
+		hshRarS3=string.sub(hshRarS, 6,6)
+	elseif qioS==1 then
+		hshRarS1=string.sub(hshRarS, 4,4)
+		hshRarS2=string.sub(hshRarS, 3,3)
+		hshRarS3=string.sub(hshRarS, 6,6)
+	elseif qioS==2 then
+		hshRarS1=string.sub(hshRarS, 4,4)
+		hshRarS2=string.sub(hshRarS, 6,6)
+		hshRarS3=string.sub(hshRarS, 3,3)
+	elseif qioS==3 then
+		hshRarS1=string.sub(hshRarS, 3,3)
+		hshRarS2=string.sub(hshRarS, 6,6)
+		hshRarS3=string.sub(hshRarS, 4,4)
+	elseif qioS==4 then
+		hshRarS1=string.sub(hshRarS, 1,1)
+		hshRarS2=string.sub(hshRarS, 6,6)
+		hshRarS3=string.sub(hshRarS, 4,4)
+	elseif qioS==5 then
+		hshRarS1=string.sub(hshRarS, 1,1)
+		hshRarS2=string.sub(hshRarS, 6,6)
+		hshRarS3=string.sub(hshRarS, 3,3)
+	elseif qioS==6 then
+		hshRarS1=string.sub(hshRarS, 6,6)
+		hshRarS2=string.sub(hshRarS, 1,1)
+		hshRarS3=string.sub(hshRarS, 3,3)
+	elseif qioS==7 then
+		hshRarS1=string.sub(hshRarS, 6,6)
+		hshRarS2=string.sub(hshRarS, 3,3)
+		hshRarS3=string.sub(hshRarS, 1,1)
+	elseif qioS==8 then
+		hshRarS1=string.sub(hshRarS, 4,4)
+		hshRarS2=string.sub(hshRarS, 5,5)
+		hshRarS3=string.sub(hshRarS, 6,6)
+	elseif qioS==9 then
+		hshRarS1=string.sub(hshRarS, 5,5)
+		hshRarS2=string.sub(hshRarS, 1,1)
+		hshRarS3=string.sub(hshRarS, 3,3)
+	end
+	hshRarSMshRezult=hshRarS1 .. hshRarS2 .. hshRarS3
+	hshMioNomeSRezult=hshMioNomeS1 .. hshMioNomeS2 .. hshMioNomeS3
+	return hshRarSMshRezult, hshMioNomeSRezult
+end
+
+
+function hshSenderNomeServ(senderNomeServ)
+	local nomeLen
+	local nome1
+	local nome11
+	local hNome
+	local hsSenderNomeServ1
+	local hsSenderNomeServ2
+	local hsSenderNomeServ3
+	local hshSenderRServ1
+	local hshSenderRServ2
+	local hshSenderRServ3
+	local hsSenderNomeRezult
+	nomeLen=string.utf8len(senderNomeServ)
+	nome1={}
+	for startLen=1,nomeLen do
+		nome1[startLen]=string.utf8sub(senderNomeServ, startLen, startLen)
+		tmNome=nome1[startLen]
+		nome1[startLen]=alfabet(tmNome)
+	end
+	nome11=0
+	for startLen=1,nomeLen do
+		nome11=nome11+nome1[startLen]
+	end
+	nome11=nome11 % 1000
+	hNomeServ=string.format("%03d",nome11)
+	hsSenderNomeServ1=string.sub(hNomeServ,1,1)
+	hsSenderNomeServ2=string.sub(hNomeServ,2,2)
+	hsSenderNomeServ3=string.sub(hNomeServ,3,3)
+	hshSenderRServ1 = math.random(0, 9)
+	hshSenderRServ2 = math.random(0, 9)
+	hshSenderRServ3 = math.random(0, 9)
+	if hshSenderRServ2==0 then
+	hsSenderNomeServRezult=hshSenderRServ1 .. hshSenderRServ2 .. hsSenderNomeServ1 .. hsSenderNomeServ2 .. hshSenderRServ3 .. hsSenderNomeServ3
+	elseif hshSenderRServ2==1 then
+		hsSenderNomeServRezult=hshSenderRServ1 .. hshSenderRServ2 .. hsSenderNomeServ2 .. hsSenderNomeServ1 .. hshSenderRServ3 .. hsSenderNomeServ3
+	elseif hshSenderRServ2==2 then
+		hsSenderNomeServRezult=hshSenderRServ1 .. hshSenderRServ2 .. hsSenderNomeServ3 .. hsSenderNomeServ1 .. hshSenderRServ3 .. hsSenderNomeServ2
+	elseif hshSenderRServ2==3 then
+		hsSenderNomeServRezult=hshSenderRServ1 .. hshSenderRServ2 .. hsSenderNomeServ1 .. hsSenderNomeServ3 .. hshSenderRServ3 .. hsSenderNomeServ2
+	elseif hshSenderRServ2==4 then
+		hsSenderNomeServRezult=hsSenderNomeServ1 .. hshSenderRServ2 .. hshSenderRServ1 .. hsSenderNomeServ3 .. hshSenderRServ3 .. hsSenderNomeServ2
+	elseif hshSenderRServ2==5 then
+		hsSenderNomeServRezult=hsSenderNomeServ1 .. hshSenderRServ2 .. hsSenderNomeServ3 .. hshSenderRServ1 .. hshSenderRServ3 .. hsSenderNomeServ2
+	elseif hshSenderRServ2==6 then
+		hsSenderNomeServRezult=hsSenderNomeServ2 .. hshSenderRServ2 .. hsSenderNomeServ3 .. hshSenderRServ1 .. hshSenderRServ3 .. hsSenderNomeServ1
+	elseif hshSenderRServ2==7 then
+		hsSenderNomeServRezult=hsSenderNomeServ3 .. hshSenderRServ2 .. hsSenderNomeServ2 .. hshSenderRServ3 .. hshSenderRServ1 .. hsSenderNomeServ1
+	elseif hshSenderRServ2==8 then
+		hsSenderNomeServRezult=hshSenderRServ1 .. hshSenderRServ2 .. hshSenderRServ3 .. hsSenderNomeServ1 .. hsSenderNomeServ2 .. hsSenderNomeServ3
+	elseif hshSenderRServ2==8 then
+		hsSenderNomeServRezult=hsSenderNomeServ1 .. hshSenderRServ2 .. hsSenderNomeServ3 .. hshSenderRServ1 .. hsSenderNomeServ2 .. hshSenderRServ3
+	elseif hshSenderRServ2==9 then
+		hsSenderNomeServRezult=hsSenderNomeServ2 .. hshSenderRServ2 .. hsSenderNomeServ3 .. hshSenderRServ1 .. hsSenderNomeServ1 .. hshSenderRServ3
+	end
+	return hsSenderNomeServRezult
+end
